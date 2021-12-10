54802390df683a411c614ee1c1160654c7022940
Pablo <pablete.arino@gmail.com>
2021-12-10 18:50:50 +0100

    finish fit method

--- dlfs/sequential.py
+++ dlfs/sequential.py
@@ -1,11 +1,13 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
-from typing import List
+from typing import List, Dict
+from tqdm import tqdm
 
 
 from dlfs.layers import Layer, Input
 from dlfs.optimizers.optimizer import Optimizer
 from dlfs.loss_functions.loss_function import LossFunction
+from dlfs.metrics import Metric, get_metric
 
 
 class Sequential:
@@ -17,7 +19,7 @@ class Sequential:
     name: str
     loss: LossFunction or None
     optimizer: Optimizer or None
-    metrics: List[str] or None
+    metrics: Dict[str, Metric] or None
     trainable: bool
 
     def __init__(self, layers: List[Layer] = None, name: str = "Sequential Model"):
@@ -59,11 +61,10 @@ class Sequential:
             metrics: the metrics to use. The metrics by default are the ones used by the loss function
             Allowed values are:
                 - "accuracy"
-                - "binary_accuracy"
         """
         self.optimizer = optimizer
         self.loss = loss
-        self.metrics = [] if metrics is None else metrics
+        self.metrics = {} if metrics is None else {metric: get_metric(metric) for metric in metrics}
 
     def summary(self):
         """
@@ -76,42 +77,38 @@ class Sequential:
             print("-" * len(layer.summary()))
             print(f"{layer.summary()}")
 
-    def backpropagation(self, x: np.ndarray, y: np.ndarray):
+    def get_gradients(self, y_pred: np.ndarray, y_true: np.ndarray) -> List[np.ndarray]:
         """
+        Backpropagation of the loss function
         Args:
-            x: the input data
-            y: the labels
+            y_pred: the predictions
+            y_true: the true labels
 
         Returns:
-            gradients: the gradients of the loss with respect to the weights and biases
+            the gradients of the loss function
         """
-
-        # forward pass
-        for layer in self.layers:
-            x = layer.forward(x)
-
         # backward pass
         # initialize the gradients
         gradients = [np.ndarray([])] * len(self.layers)
-        gradients[-1] = self.loss.backward(x, y)
+        gradients[-1] = self.loss.backward(y_pred, y_true)
 
         # compute the gradients
         for i in range(len(self.layers) - 1, 0, -1):
             gradients[i - 1] = self.layers[i].backward(gradients[i])
-            gradients[i] = self.layers[i].backward(gradients[i + 1])
 
         return gradients
 
     def fit(self, x: np.ndarray, y: np.ndarray, epochs: int = 1, batch_size: int = 32, verbose: int = 1,
             validation_data: np.ndarray = None, validation_split: float = 0.0, shuffle: bool = True, initial_epoch=0):
         """
+        Fit the model to the data
 
         Args:
-            x: the input data
+            x: the input data. With shape (n_samples, n_features)
             y: the labels
             epochs: the number of epochs to train the model
             batch_size: the batch size
-            verbose: the verbosity mode
+            verbose: the verbosity mode. 0 is progress bar, 1 is one line per epoch, 2 is one line per batch
             validation_data: the validation data
             validation_split: the validation split
             shuffle: whether to shuffle the data
@@ -121,71 +118,102 @@ class Sequential:
         if self.optimizer is None or self.loss is None:
             raise ValueError("You must compile the model before training")
 
+        using_validation_data = False
         # if validation data is not provided, split the data into train and validation
         if validation_split > 0:
-            if validation_data is None:
-                raise ValueError("If validation_split is set, validation_data must be specified")
+            if validation_data is not None:
+                raise ValueError("validation_data and validation_split cannot be used together")
             x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=validation_split)
+            using_validation_data = True
+
+        if validation_data is not None:
+            x_val, y_val = validation_data
+            using_validation_data = True
+
+        if using_validation_data and verbose == 0:
+            raise ValueError("validation_data and verbose=0 cannot be used together")
+
+        # if the layers are not initialized, initialize them
+        if not self.layers[0].initialized:
+            self.layers[0].initialize(input_shape=(batch_size, *x.shape[1:]))
+
+        for i in range(1, len(self.layers)):
+            self.layers[i].initialize(input_shape=self.layers[i - 1].output_shape)
 
         # initialize the history
         history = {'loss': [], 'val_loss': []}
-
         for metric in self.metrics:
             history[metric] = []
             history['val_' + metric] = []
 
+        # create the progress bar if verbose is 0
+        r = range(initial_epoch, epochs)
+        range_epochs = r if verbose > 0 else tqdm(r)
+
         # loop over the number of epochs
-        for epoch in range(initial_epoch, epochs):
-            # initialize the total loss for the epoch
-            epoch_loss = 0.0
-            if 'accuracy' in self.metrics:
-                epoch_acc = 0.0
-            if 'binary_accuracy' in self.metrics:
-                epoch_binary_acc = 0.0
+        for epoch in range_epochs:
+            # initialize the total loss for the epoch if verbose is 2
+            if verbose == 2:
+                epoch_loss = 0.0
+                epoch_metrics = {metric: 0.0 for metric in self.metrics}
 
             # loop over the data in batches
             for x_batch, y_batch in self.batch_generator(x, y, batch_size, shuffle):
 
-                # get the gradients for the batch
-                grads = self.backpropagation(x_batch, y_batch)
+                # forward pass
+                y_pred = self.predict(x_batch, training=True)
+
+                # backward pass: get the gradients
+                gradients = self.get_gradients(y_pred, y_batch)
 
                 # update the parameters
-                self.optimizer.update_params(self, grads)
+                self.optimizer.update_params(self, gradients)
 
                 # compute the loss for the batch
-                loss = self.loss.compute_loss(self, x_batch, y_batch)
-                # update the total loss
-                epoch_loss += loss
+                loss = self.loss.compute_loss(self, y_pred, y_batch)
 
-                # compute the metrics
-                if 'accuracy' in self.metrics:
-                    acc = self.metrics['accuracy'].compute_metric(self, x_batch, y_batch)
-                    epoch_acc += acc
-                # update the history
-                history['loss'].append(loss)
-                if 'accuracy' in self.metrics:
-                    history['accuracy'].append(acc)
-            # compute the average loss for the epoch
-            epoch_loss /= len(x)
-            if 'acc' in self.metrics:
-                epoch_acc /= len(x)
-            # update the history
-            history['val_loss'].append(epoch_loss)
-            if 'acc' in self.metrics:
-                history['val_acc'].append(epoch_acc)
-            # print the metrics
-            if verbose:
+                # update the total loss (if verbose is 2)
+                if verbose == 2:
+                    epoch_loss += loss
+                    for metric in self.metrics:
+                        epoch_metrics[metric] += self.metrics[metric].compute_metric(self, x_batch, y_batch)
+
+                # print the loss and the metrics per batch (if verbose is 1)
+                if verbose == 1:
+                    print(f"Epoch {epoch + 1}/{epochs}: loss = {loss:.4f}")
+                    for metric in self.metrics:
+                        print(f"\t{metric} = {self.metrics[metric].compute_metric(self, y_pred, y_batch):.4f}")
+
+            # print the metrics (if verbose is 1 or 2)
+            if verbose > 0:
                 print(f"Epoch {epoch + 1}/{epochs}")
                 print(f"\tTrain loss: {epoch_loss}")
+                for metric in self.metrics:
+                    print(f"\t{metric}: {epoch_metrics[metric] / len(x)}")
 
-                # TODO: print the metrics
-
-                if validation_data is not None:
-                    val_loss = self.loss.compute_loss(self, x_val, y_val)
-                    val_acc = self.metrics['acc'].compute_metric(self, x_val, y_val)
+                if using_validation_data:
+                    y_pred_val = self.predict(x_val)
+                    val_loss = self.loss.compute_loss(self, y_pred_val, y_val)
                     print(f"\tValidation loss: {val_loss}")
-                    print(f"\tValidation acc: {val_acc}")
-                print("")
+                    # add the validation loss to the history
+                    history['val_loss'].append(epoch_loss)
+
+                    for metric in self.metrics:
+                        val_metric = self.metrics[metric].compute_metric(self, x_val, y_val)
+                        print(f"\t{metric}: {val_metric:.4f}")
+                        # add the validation metric to the history
+                        history['val_' + metric].append(val_metric)
+
+                    # append the val_loss and val_metrics to the history
+                    history['val_loss'].append(val_loss)
+                    for metric in self.metrics:
+                        history['val_' + metric].append(self.metrics[metric].compute_metric(self, y_pred_val, y_val))
+            print("")
+
+            # save the history
+            history['loss'].append(epoch_loss)
+            for metric in self.metrics:
+                history[metric].append(epoch_metrics[metric] / len(x))
 
         return history
 
@@ -247,7 +275,7 @@ class Sequential:
             pass
         return total_loss
 
-    def predict(self, x: np.ndarray, batch_size: int = 32, verbose: int = 1):
+    def predict(self, x: np.ndarray, batch_size: int = 32, verbose: int = 1, training: bool = False) -> np.ndarray:
         """
 
         Args:
