40b60da4cb9cf3010e30d6e868577bd05e7074c9
Pablo <pablete.arino@gmail.com>
2021-12-10 19:07:54 +0100

    add docstrings

--- dlfs/layers/flatten.py
+++ dlfs/layers/flatten.py
@@ -4,17 +4,33 @@ from .layer import Layer
 
 
 class Flatten(Layer):
+    """
+    Flatten layer. It flattens the input to a 1D vector.
+    """
 
-    def __init__(self, name="Flatten"):
+    def __init__(self, name: str = "Flatten"):
 
         super(Flatten, self).__init__(name=name)
 
     def initialize(self, input_shape: tuple):
+        """
+        Initialize the layer.
+        Args:
+            input_shape: The input shape.
+        """
         self.input_shape = input_shape
         self.output_shape = (input_shape[0], np.prod(input_shape[1:]))
         self.initialized = True
 
-    def forward(self, x: np.ndarray) -> np.ndarray:
+    def forward(self, x: np.ndarray, training: bool = True) -> np.ndarray:
+        """
+        Forward pass of the layer.
+        Args:
+            x: Input to the layer.
+            training: For compatibility with the base class.
+        Returns:
+            A 1D vector.
+        """
         return np.reshape(x, self.output_shape)
 
     def backward(self, gradients: np.ndarray) -> np.ndarray:
