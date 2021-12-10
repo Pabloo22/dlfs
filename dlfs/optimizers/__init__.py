7a81524b07fc2a5e4abcb3d90b42f4491a08be0d
Pablo <pablete.arino@gmail.com>
2021-12-10 18:59:09 +0100

    add sgd optimizer

--- dlfs/optimizers/__init__.py
+++ dlfs/optimizers/__init__.py
@@ -1,2 +1,4 @@
 from .adam import Adam
+from .sgd import SGD
+from .optimizer import Optimizer
 from .utils import get_optimizer
---
+++ dlfs/optimizers/sgd.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+from dlfs.optimizers import Optimizer
+
+
+class SGD(Optimizer):
+
+    def __init__(self, lr: float = 0.01):
+        super(SGD, self).__init__(learning_rate=lr)
+
+    def update(self, params: np.ndarray, grads: np.ndarray):
+        """
+        Update parameters using SGD
+
+        Args:
+            params (np.ndarray): parameters to be updated (weights and bias)
+            grads (np.ndarray): gradients of parameters
+        """
+        w, b = params
+        dw, db = grads
+
+        w -= self.learning_rate * dw
+        b -= self.learning_rate * db
