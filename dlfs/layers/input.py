786cbcd470c1d2a7ad974be1e8e1eae0c7d58e3b
Pablo <pablete.arino@gmail.com>
2021-12-11 10:55:46 +0100

    add docstrings and update method

--- dlfs/layers/input.py
+++ dlfs/layers/input.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from .layer import Layer
+from dlfs.optimizers import Optimizer
 
 
 class Input(Layer):
@@ -10,19 +11,44 @@ class Input(Layer):
         self.input = None
         self.output = None
 
-    def initialize(self, input_shape: tuple):
+    def initialize(self, input_shape: tuple, optimizer: Optimizer = None):
         """
         Implemented for consistency with other layers.
         """
         self.initialized = True
 
-    def forward(self, x: np.ndarray):
+    def forward(self, x: np.ndarray, training: bool = True) -> np.ndarray:
+        """
+        Implemented for consistency with other layers.
+
+        Args:
+            x: Input to the layer.
+            training: Whether the model is in training mode.
+
+        Returns:
+            Output of the layer (same as input).
+        """
         self.input = x
         self.output = x
         return x
 
-    def backward(self, gradients: np.ndarray):
+    def backward(self, gradients: np.ndarray) -> np.ndarray:
+        """
+        Implemented for consistency with other layers.
+
+        Args:
+            gradients: Gradients of the loss function with respect to the output of this layer.
+
+        Returns:
+            Gradients of the loss function with respect to the input of this layer.
+        """
         return gradients
+
+    def update(self, gradients: np.ndarray):
+        """
+        Implemented for consistency with other layers.
+        """
+        pass
 
     def summary(self) -> str:
         return f"Input: {self.input_shape}"
