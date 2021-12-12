bc7721afd5a0a8e0882941fd444844de94c1b5b5
Pablo <pablete.arino@gmail.com>
2021-12-12 11:37:59 +0100

    add tanh

--- dlfs/activation_functions/__init__.py
+++ dlfs/activation_functions/__init__.py
@@ -2,4 +2,5 @@ from .activation_function import ActivationFunction
 from .sigmoid import Sigmoid
 from .softmax import Softmax
 from .relu import ReLU
+from .linear import Linear
 from .utils import get_activation_function
