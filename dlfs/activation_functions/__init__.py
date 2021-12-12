1f0d119fe4b7cc8b81ca4057fa2538a5bd99bea0
Pablo <pablete.arino@gmail.com>
2021-12-12 11:42:13 +0100

    add tanh

--- dlfs/activation_functions/__init__.py
+++ dlfs/activation_functions/__init__.py
@@ -3,4 +3,5 @@ from .sigmoid import Sigmoid
 from .softmax import Softmax
 from .relu import ReLU
 from .linear import Linear
+from .tanh import Tanh
 from .utils import get_activation_function
--- dlfs/activation_functions/utils.py
+++ dlfs/activation_functions/utils.py
@@ -1,22 +1,34 @@
-from .activation_function import ActivationFunction
-from .relu import ReLU
-from .softmax import Softmax
+from typing import Union
+
+from . import ActivationFunction, Sigmoid, ReLU, Softmax, Linear, Tanh
 
 
-def get_activation_function(activation_function_name: str) -> ActivationFunction:
+def get_activation_function(activation_function_name: Union[str, None]) -> Union[ActivationFunction, None]:
     """
     Returns an activation function object based on the name of the activation function.
     Args:
         activation_function_name: The name of the activation function. The possible names are:
             - "relu": ReLU
             - "softmax": Softmax
+            - "sigmoid": Sigmoid
+            - "tanh": Tanh
+            - "linear": Linear
+            - None: No activation function (i.e. the input is passed through, the same as Linear)
     Returns:
-        An activation function object.
+        An activation function object or None if there is no activation function (the same as using 'Linear').
     """
+    if activation_function_name is None:
+        return None
 
     if activation_function_name == 'relu':
         return ReLU()
     elif activation_function_name == 'softmax':
         return Softmax()
+    elif activation_function_name == 'sigmoid':
+        return Sigmoid()
+    elif activation_function_name == 'linear':
+        return Linear()
+    elif activation_function_name == 'tanh':
+        return Tanh()
     else:
-        raise ValueError('Activation function name not recognized')
+        raise ValueError(f'Unknown activation function name: {activation_function_name}')
