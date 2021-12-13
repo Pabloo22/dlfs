7cf82130c03e6ffe75f78fb1f8dbfe9ff3033eb2
Pablo <pablete.arino@gmail.com>
2021-12-13 15:03:39 +0100

    update get_optimizer()

--- dlfs/optimizers/utils.py
+++ dlfs/optimizers/utils.py
@@ -1,20 +1,27 @@
-from .adam import Adam
-from .optimizer import Optimizer
+from . import Optimizer, SGD, SGDMomentum, Adam, Adagrad
 
 
-def get_optimizer(optimizer_name: str, learning_rate: float, **kwargs) -> Optimizer:
+def get_optimizer(optimizer_name: str) -> Optimizer:
     """
     Get an optimizer by name.
 
     Args:
-        optimizer_name (str): The name of the optimizer.
-        learning_rate (float): The learning rate.
-        **kwargs: Additional arguments for the optimizer.
+        optimizer_name (str): The name of the optimizer. Valid names are:
+            - sgd
+            - sgd_momentum
+            - adam
+            - adagrad
 
     Returns:
         The optimizer.
     """
-    if optimizer_name == 'adam':
-        return Adam(learning_rate, **kwargs)
+    if optimizer_name == "sgd":
+        return SGD()
+    elif optimizer_name == "sgd_momentum":
+        return SGDMomentum()
+    elif optimizer_name == "adam":
+        return Adam()
+    elif optimizer_name == "adagrad":
+        return Adagrad()
     else:
-        raise ValueError('Invalid optimizer name')
+        raise ValueError(f"Unknown optimizer name: {optimizer_name}")
