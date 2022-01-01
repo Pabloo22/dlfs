1be34d805ad72b15bbbadeb211c16baf708ab72e
Pablo <pablete.arino@gmail.com>
2022-01-01 19:40:12 +0100

    fix update method

--- dlfs/optimizers/sgd.py
+++ dlfs/optimizers/sgd.py
@@ -1,4 +1,5 @@
 import numpy as np
+from typing import Tuple
 
 from dlfs.optimizers import Optimizer
 
@@ -11,25 +12,20 @@ class SGD(Optimizer):
     def __init__(self, lr: float = 0.01):
         super(SGD, self).__init__(learning_rate=lr)
 
-    def update(self, params: np.ndarray, grads: np.ndarray):
+    def update(self, parameters: Tuple[np.ndarray, np.ndarray],
+               gradients: Tuple[np.ndarray, np.ndarray]):
         """
         Update parameters using SGD
 
         Args:
-            params (np.ndarray): parameters to be updated (weights and bias)
-            grads (np.ndarray): gradients of parameters
+            parameters (np.ndarray): parameters to be updated (weights and bias)
+            gradients (np.ndarray): gradients of parameters (weights and bias)
         """
-        w, b = params
-        # The sum is squashing the results down to a single update
-        db = np.sum(grads, axis=0, keepdims=True)
-        # take the average of the gradients if there are multiple
-        if len(grads.shape) > 1:
-            dw = np.sum(grads, axis=0, keepdims=True) / grads.shape[0]
-        else:
-            dw = np.sum(grads, axis=0, keepdims=True)
+        w, b = parameters
+        dw, db = gradients
 
-        # if dw.shape != w.shape:
-        #     raise ValueError(f"Shape of dw and w do not match: {dw.shape} != {w.shape}")
+        if dw.shape != w.shape:
+            raise ValueError(f"Shape of dw and w do not match: {dw.shape} != {w.shape}")
 
         w -= self.learning_rate * dw
         b -= self.learning_rate * db
