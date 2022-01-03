a1aa1e30fab935243597f04e96694ebc3e0aa351
Pablo <pablete.arino@gmail.com>
2022-01-03 17:33:13 +0100

    rename convolve_2d to simple_convolution

--- dlfs/layers/conv.py
+++ dlfs/layers/conv.py
@@ -145,74 +145,10 @@ class Conv2D(Layer):
         self.output_shape = self._get_output_shape()
         self.initialized = True
 
-    def forward(self, x: np.ndarray, training: bool = False) -> np.ndarray:
-        """
-        Forward pass
-        Args:
-            x: input data
-            training: for compatibility with other layers
-        Returns:
-            output data
-        """
-
-        output = np.zeros((x.shape[0], x.shape[1], x.shape[2], self.__n_filters))
-        for i in range(x.shape[0]):
-            for j in range(x.shape[1]):
-                for k in range(x.shape[2]):
-                    for L in range(self.__n_filters):
-                        output[i, j, k, L] = np.sum(
-                            x[i, j:j + self.__kernel_size[0], k:k + self.__kernel_size[1]] * self.__weights[L]) + \
-                                             self.__bias[L]
-
-        self.__input = x
-        self.__output = output
-        return output
-
-    def get_delta(self, last_delta: np.ndarray, dz_da: np.ndarray) -> np.ndarray:
-        """
-        Backward pass
-        Args:
-            last_delta: gradients of the loss with respect to the output of this layer
-            derivative of the output of this layer (i) with respect to the z of the next layer (i+1)
-        Returns:
-            The corresponding delta of the layer (d_cost/d_z).
-        """
-        x = self.__input
-        # the input x has the shape (batch_size, height, width, n_filters)
-        last_delta = last_delta.reshape((x.shape[0], x.shape[1], x.shape[2], self.__n_filters))
-        gradients_w = np.zeros(self.__weights.shape)
-        gradients_b = np.zeros(self.__bias.shape)
-        for i in range(x.shape[0]):  # batch
-            for j in range(x.shape[1]):  # height
-                for k in range(x.shape[2]):  # width
-                    for L in range(self.__n_filters):  # filters
-                        gradients_w[L] += np.sum(
-                            x[i, j:j + self.__kernel_size[0], k:k + self.__kernel_size[1]] * last_delta[i, j, k, L])
-                        gradients_b[L] += last_delta[i, j, k, L]
-
-        return self.activation.gradient(gradients_w), gradients_b
-
-    def count_params(self) -> int:
-        """
-        Counts the number of parameters of this layer
-        Returns:
-            number of parameters
-        """
-        return self.__weights.size + self.__bias.size
-
-    def _get_output_shape(self) -> tuple:
-        """
-        Get the output shape
-        Returns:
-            tuple: output shape
-        """
-        return (self.input_shape[0], self.input_shape[1] - self.__kernel_size[0] + 1,
-                self.input_shape[2] - self.__kernel_size[1] + 1, self.__n_filters)
-
     @staticmethod
     def simple_convolution(image: np.ndarray, kernel: np.ndarray, padding: bool = False, stride: int = 1) -> np.ndarray:
         """
-        Performs a valid convolution on an image with a kernel.
+        Performs a valid convolution on an image (with only a channel) with a kernel.
 
         Args:
             image: A grayscale image.
@@ -242,9 +178,101 @@ class Conv2D(Layer):
             for j in range(output_width):
                 convolved_image[i, j] = np.sum(
                     image[i * stride:i * stride + kernel_height,
-                          j * stride:j * stride + kernel_width] * kernel)
+                    j * stride:j * stride + kernel_width] * kernel)
 
         return convolved_image
+
+    @staticmethod
+    def convolve(image: np.ndarray,
+                 kernel: np.ndarray,
+                 bias: np.ndarray = None,
+                 padding: bool = False,
+                 stride: int = 1) -> np.ndarray:
+        """
+        Performs a valid convolution to an image with a kernel.
+
+        Args:
+            image: An image with multiple channels.
+            kernel: A kernel tensor of shape (n_channels, kernel_height, kernel_width).
+            bias: A bias tensor of shape (n_channels,).
+            padding: Whether to pad the image.
+            stride: convolution stride size.
+
+        Returns:
+            A tensor of shape (n_channels, output_height, output_width).
+        """
+
+        # Get the dimensions of the image and kernel
+        n_channels, image_height, image_width = image.shape
+        kernel_height, kernel_width = kernel.shape[1:]
+
+        # Pad the image if padding is True
+        if padding:
+            image = np.pad(image, ((0, 0), (kernel_height // 2, kernel_height // 2),
+                                   (kernel_width // 2, kernel_width // 2)),
+                           mode='constant', constant_values=0)
+
+        # Create the output image
+        output_height = (image_height - kernel_height) // stride + 1
+        output_width = (image_width - kernel_width) // stride + 1
+
+        # Initialize the output tensor
+        convolved_image = np.zeros((n_channels, output_height, output_width))
+
+        # Perform the convolution
+
+
+
+    def forward(self, x: np.ndarray, training: bool = False) -> np.ndarray:
+        """
+        Forward pass
+        Args:
+            x: input data
+            training: for compatibility with other layers
+        Returns:
+            output data
+        """
+
+        output = np.zeros((x.shape[0], x.shape[1], x.shape[2], self.__n_filters))
+        for i in range(x.shape[0]):
+            for j in range(x.shape[1]):
+                for k in range(x.shape[2]):
+                    for L in range(self.__n_filters):
+                        output[i, j, k, L] = np.sum(
+                            x[i, j:j + self.__kernel_size[0], k:k + self.__kernel_size[1]] * self.__weights[L]) + \
+                                             self.__bias[L]
+
+        self.__input = x
+        self.__output = output
+        return output
+
+    def get_delta(self, last_delta: np.ndarray, dz_da: np.ndarray) -> np.ndarray:
+        """
+        Backward pass
+        Args:
+            last_delta: gradients of the loss with respect to the output of this layer
+            dz_da: derivative of the z of the next layer (i+1) with respect to the activation of the current layer (i)
+        Returns:
+            The corresponding delta of the layer (d_cost/d_z).
+        """
+
+
+    def count_params(self) -> int:
+        """
+        Counts the number of parameters of this layer
+        Returns:
+            number of parameters
+        """
+        return self.__weights.size + self.__bias.size
+
+    def _get_output_shape(self) -> tuple:
+        """
+        Get the output shape
+        Returns:
+            tuple: output shape
+        """
+        return (self.input_shape[0], self.input_shape[1] - self.__kernel_size[0] + 1,
+                self.input_shape[2] - self.__kernel_size[1] + 1, self.__n_filters)
 
     def compute_weights_gradients(self, gradients: np.ndarray) -> np.ndarray:
         """
