e72e7c028b362ed3f521eef1ee0c5dc09929a545
Pablo <pablete.arino@gmail.com>
2022-01-04 13:54:57 +0100

    redesign class Optimizer

--- dlfs/optimizers/optimizer.py
+++ dlfs/optimizers/optimizer.py
@@ -2,28 +2,29 @@ from abc import ABC, abstractmethod
 import numpy as np
 from typing import Tuple
 
+from dlfs.layers import Layer
+
 
 class Optimizer(ABC):
     """Base class for all optimizers."""
 
     def __init__(self, learning_rate):
-        self.__learning_rate = learning_rate
-
-    @property
-    def learning_rate(self):
-        return self.__learning_rate
-
-    @learning_rate.setter
-    def learning_rate(self, learning_rate):
-        self.__learning_rate = learning_rate
+        self.learning_rate = learning_rate
 
     @abstractmethod
-    def update(self, parameters: Tuple[np.ndarray, np.ndarray],
-               gradients: Tuple[np.ndarray, np.ndarray]):
+    def update(self, layer: Layer, gradients: Tuple[np.ndarray, np.ndarray]):
         """Update parameters based on gradients.
         Args:
-            parameters: Parameters to update passed as a tuple of two numpy arrays composed of the weights and biases.
+            layer: The layer to update.
             gradients: Gradients of the parameters passed as a tuple of two numpy arrays composed of the gradients of
                 the weights and biases.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def add_slot(self, layer: Layer):
+        """Add a slot to the optimizer.
+        Args:
+            layer: The layer to add a slot to.
         """
         raise NotImplementedError
