cd18189b777533114fd0eda236736394c8d6897b
Pablo <pablete.arino@gmail.com>
2022-01-08 12:54:25 +0100

    fix accuracy

--- dlfs/metrics/accuracy.py
+++ dlfs/metrics/accuracy.py
@@ -20,4 +20,11 @@ class Accuracy(Metric):
         Returns:
             The accuracy of the model.
         """
-        return np.mean(np.argmax(y_true, axis=1) == np.argmax(y_pred, axis=1))
+        using_multi_label = len(y_true[0]) > 1
+        y_pred = np.argmax(y_pred, axis=1) if using_multi_label else np.round(y_pred)
+        y_true = np.argmax(y_true, axis=1) if using_multi_label else y_true
+
+        # Return the accuracy of the model (multilabel or binary)
+        accuracy = np.mean(y_pred == y_true)
+        return accuracy
+
