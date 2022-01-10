d409976e1f30e707d4082a363a8e6cec30ef8057
Pablo <pablete.arino@gmail.com>
2022-01-10 16:41:54 +0100

    update Dropout layer

--- dlfs/layers/dropout.py
+++ dlfs/layers/dropout.py
@@ -6,33 +6,53 @@ from dlfs.optimizers import Optimizer
 
 class Dropout(Layer):
 
-    def __init__(self, p=0.5, name="Dropout"):
+    def __init__(self, p: float = 0.5, name: str = "Dropout"):
         super(Dropout, self).__init__(name=name, has_weights=False)
-        self.p = 1 - p  # probability of keeping a neuron active
+        self.prob_keep = 1 - p  # probability of keeping a neuron active
         self.mask = None
 
     def initialize(self, input_shape: tuple):
+        """
+        Initialize input shape and output shape.
+        Args:
+            input_shape: shape of input data
+        """
         self.input_shape = input_shape
         self.output_shape = input_shape
         self.initialized = True
 
-    def forward(self, x, training=True):
+    def forward(self, x: np.ndarray, training: bool = True):
+        """
+        Forward pass of the layer.
+        Args:
+            x: input to the layer.
+            training: whether the layer is in training mode.
+        Returns:
+            output of the layer.
+        """
         if training:
-            self.mask = np.random.binomial(1, self.p, x.shape) / self.p
+            self.mask = np.random.binomial(1, self.prob_keep, x.shape) / self.prob_keep
             return x * self.mask
         else:
             return x
 
-    def get_delta(self, last_delta, dz_da):
-        return last_delta @ dz_da
-
-    def get_dz_da(self):
-        return self.mask
+    def get_d_inputs(self, delta: np.ndarray) -> np.ndarray:
+        """
+        Returns the derivative of the cost function with respect to the input of the layer.
+        Args:
+            delta: derivative of the cost function with respect to the output of the layer.
+        Returns:
+            derivative of the cost function with respect to the input of the layer.
+        """
+        return delta * self.mask
 
     def summary(self):
-        return f"{self.name} (p={1 - self.p})"
+        return f"{self.name} (p={1 - self.prob_keep})"
 
     def update(self, optimizer: Optimizer, gradients: np.ndarray):
+        """
+        Implemented for compatibility with the Layer interface.
+        """
         pass
 
     def count_params(self) -> int:
