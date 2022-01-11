e38290013a98f9220de2be0224f84e7d58940fcc
Pablo <pablete.arino@gmail.com>
2022-01-11 20:35:34 +0100

    fix license name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='dlfs',  # How you named your package folder (MyLib)
     packages=['dlfs'],  # Chose the same as "name"
     version='0.1',  # Start with a small number and increase it with every change you make
-    license='Apache-2.0',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+    license='apache-2.0',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description='implement from scratch (using numpy arrays) a package based on tensorflow architecture which '
                 'allows to build and train Fully Connected Networks and Convolutional Neural Networks (CNNs).',
     author='Pablo',  # Type in your name
