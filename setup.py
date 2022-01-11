ec9d2dca1af1b33a956a7e4a2bb0a2b2b4f5c1c5
Pablo <pablete.arino@gmail.com>
2022-01-11 20:38:38 +0100

    fix license classifier

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
         # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: Apache-2.0 License',
+        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
