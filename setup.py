62733782b7a1a1caa56142fc857c506311442bbf
Pablo <pablete.arino@gmail.com>
2022-01-11 20:42:36 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from distutils.core import setup
 setup(
     name='dlfs',  # How you named your package folder (MyLib)
     packages=['dlfs'],  # Chose the same as "name"
-    version='0.1',  # Start with a small number and increase it with every change you make
+    version='0.1.1',  # Start with a small number and increase it with every change you make
     license='apache-2.0',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description='implement from scratch (using numpy arrays) a package based on tensorflow architecture which '
                 'allows to build and train Fully Connected Networks and Convolutional Neural Networks (CNNs).',
     author='Pablo',  # Type in your name
     author_email='pablete.arino@gmail.com',
     url='https://github.com/Pabloo22/Deep-Learning-from-Scratch',
-    download_url='https://github.com/Pabloo22/dlfs/archive/refs/tags/v0.1.tar.gz',
+    download_url='https://github.com/Pabloo22/dlfs/archive/refs/tags/v0.1.1.tar.gz',
     keywords=['machine-learning', 'deep-learning', 'backpropagation'],  # Keywords that define your package best
     install_requires=[
         'numpy',
