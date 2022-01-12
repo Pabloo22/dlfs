f1d447292c6fa9b643a90e4643ec2c6c0587841f
Pablo <pablete.arino@gmail.com>
2022-01-12 13:44:14 +0100

    create PatchConvolutioner class

---
+++ dlfs/convolutions/patch_convolutioner.py
@@ -0,0 +1,44 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Contains the PatchConvolutioner class"""
+
+import numpy as np
+from typing import Union
+
+
+from .convolutioner import Convolutioner
+
+
+class PatchConvolutioner(Convolutioner):
+    """Convolutioner for patch-based convolutions.
+
+    This class implements the convolutioner interface for patch-based convolutions. For avoid using
+    for loops (which are slow), we can take advantage of how numpy arrays are stored in memory.
+    """
+    def __init__(self,
+                 image_size: Union[int, tuple],
+                 kernel_size: Union[int, tuple],
+                 padding: Union[int, tuple] = (0, 0),
+                 stride: Union[int, tuple] = (1, 1)):
+
+        super().__init__(image_size, kernel_size, padding, stride)
+
+    @staticmethod
+    def convolve_grayscale(image: np.ndarray, kernel: np.ndarray, padding: Union[int, tuple] = (0, 0),
+                           stride: Union[int, tuple] = (1, 1), using_batches: bool = False) -> np.ndarray:
+        pass
+
+    @staticmethod
+    def convolve_multichannel(image: np.ndarray, kernel: np.ndarray, padding: tuple = (0, 0),
+                              stride: Union[int, tuple] = (1, 1), using_batches: bool = False) -> np.ndarray:
+        pass
